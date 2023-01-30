2d7eec8d93c5ae60906f88aa5d1bfd6fe63e79c2
CMING <cming.pms@gmail.com>
2023-01-30 15:38:20 +0800

    created handleChange function

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ class App extends Component {
     super();
 
     this.state={
-      name:'',
-      email:'',
-      phone:'',
-      schoolName:'',
-      schoolTitle:'',
-      schoolDate:'',
-      companyName:'',
-      companyPosition:'',
-      companyDate:''
+      info:{
+        name:'',
+        email:'',
+        phone:'',
+        schoolName:'',
+        schoolTitle:'',
+        schoolDate:'',
+        companyName:'',
+        companyPosition:'',
+        companyDate:'',
+      }
     }
   }
 
+  handleChange = (e) => {
+    const {value, id} = e.target
+    this.setState({
+      info:{
+        [id]: value,
+      }
+    })
+  }
+
   render(){
+    const {name, email, phone, schoolName, schoolTitle,
+      schoolDate, companyName, companyPosition, companyDate} = this.state.info;
+
     return(
       <div className='CV-App'> 
         <div className="left-block"> 
@@ -30,15 +44,15 @@ class App extends Component {
               <h3>General Information</h3>
               <div className="input">
                 <label htmlFor='name'>Name: </label>
-                <input type="text" id="name"></input>
+                <input onChange={this.handleChange} value={name} type="text" id="name"></input>
               </div>
               <div className="input">
                 <label htmlFor='email'>Email: </label>
-                <input type="text" id="email"></input>
+                <input onChange={this.handleChange} value={email} type="text" id="email"></input>
               </div>
               <div className="input">
                 <label htmlFor='phone'>Phone: </label>
-                <input type="text" id="phone"></input>
+                <input onChange={this.handleChange} value={phone} type="text" id="phone"></input>
               </div>
             </section>
 
@@ -46,15 +60,15 @@ class App extends Component {
               <h3>Education</h3>
               <div className="input">
                 <label htmlFor='school-name'>School name: </label>
-                <input type="text" id="school-name"></input>
+                <input onChange={this.handleChange} value={schoolName} type="text" id="schoolName"></input>
               </div>
               <div className="input">
                 <label htmlFor='school-title'>Title: </label>
-                <input type="text" id="school-title"></input>
+                <input onChange={this.handleChange} value={schoolTitle} type="text" id="schoolTitle"></input>
               </div>
               <div className="input">
                 <label htmlFor='school-date'>Date: </label>
-                <input type="text" id="school-date"></input>
+                <input onChange={this.handleChange} value={schoolDate} type="text" id="schoolDate"></input>
               </div>
             </section>
 
@@ -62,15 +76,15 @@ class App extends Component {
               <h3>Working Experience</h3>
               <div className="input">
                 <label htmlFor='company-name'>Company name: </label>
-                <input type="text" id="company-name"></input>
+                <input onChange={this.handleChange} value={companyName} type="text" id="companyName"></input>
               </div>
               <div className="input">
                 <label htmlFor='company-position'>Position: </label>
-                <input type="text" id="company-position"></input>
+                <input onChange={this.handleChange} value={companyPosition} type="text" id="companyPosition"></input>
               </div>
               <div className="input">
                 <label htmlFor='company-date'>Date: </label>
-                <input type="text" id="company-date"></input>
+                <input onChange={this.handleChange} value={companyDate} type="text" id="companyDate"></input>
               </div>
             </section>
             <input type="submit" value="submit"/>
